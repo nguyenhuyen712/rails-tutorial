318b8242cabbb26bfb5f2780693e5431915aa951
Nguyen Huyen <nguyen.my.huyen@framgia.com>
2016-10-21 08:35:51 +0700

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 20161019074428) do
+ActiveRecord::Schema.define(version: 20161020025732) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -7,6 +7,9 @@ ActiveRecord::Schema.define(version: 20161019074428) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
