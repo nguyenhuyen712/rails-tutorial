18a4c2cf5a60ffd478d2c8d8b2a192d95bca774e
Nguyen Huyen <nguyen.my.huyen@framgia.com>
2016-10-25 11:23:50 +0700

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   scope :order_by_desc, ->{order created_at: :desc}
+  scope :following_posts, ->(following_ids, user_id){where("user_id IN (?)
+    OR user_id = ?", following_ids, user_id)}
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
